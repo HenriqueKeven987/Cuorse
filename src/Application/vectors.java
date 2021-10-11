eab6eced5b76a99764376086ef3757f2f8339de9
HenriqueKeven <henrique.keven@seduc.ce.gov.br>
2021-10-11 08:06:59 -0300

    vector 0.1

--- src/Application/vectors.java
+++ src/Application/vectors.java
@@ -8,19 +8,23 @@ public class vectors {
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
-		Locale.setDefault(Locale.US);
+		//Locale.setDefault(Locale.US);
 		
-		
-		System.out.println("Quantas Alturas Voce quer tirar a media?");
+		System.out.print("Quantas Alturas Voce quer tirar a media?");
 		int vezes = entrada.nextInt();
 		double[] alturas = new double[vezes];
+		double soma = 0;
 		
-		
-		for(int i = 0;i <= vezes;i++) {
+		for(int i = 0;i < vezes;i++) {
 			
-			System.out.println("Digite a Altura "+ i); 
+			System.out.println("Digite a Altura "); 
 			alturas[i] = entrada.nextDouble();
+			soma += alturas[i];
 		}
+		
+		double media = soma / vezes;
+		
+		System.out.printf("a media de altura é %.2f", media);
 		
 		
 		entrada.close();
