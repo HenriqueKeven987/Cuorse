06ec7dccb080849fb8dee46abee307c0680d1445
HenriqueKeven <henrique.keven@seduc.ce.gov.br>
2021-10-15 14:37:58 -0300

    list 0.3

--- src/Application/ListProgram.java
+++ src/Application/ListProgram.java
@@ -2,10 +2,11 @@ package Application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 
 		List<String> lista = new ArrayList<>();
 
@@ -23,13 +24,29 @@ public class ListProgram {
 		
 		System.out.println("-----------------------");
 		
-		lista.remove(3);
+		lista.remove(0);
 		lista.removeIf(list -> list.charAt(0) == 'A');
 		
 		for (String list : lista) {
 			System.out.println(list); 
 		}
-
+		
+		System.out.println("-----------------------");
+		System.out.println(lista.indexOf("Aster"));
+		System.out.println(lista.indexOf("Jaden"));
+		
+		System.out.println("-----------------------");
+		
+		List<String> resultado = lista.stream().filter(list -> list.charAt(0) == 'J').collect(Collectors.toList());
+		for (String list2 : resultado) {
+			System.out.println(list2);
+		}
+		
+		System.out.println("----------------------");
+		String name = lista.stream().filter(qualquercoisa -> qualquercoisa.charAt(0) == 'x').findFirst().orElse(null);
+		System.out.println(name);
+		
+		
 	}
 
 }
