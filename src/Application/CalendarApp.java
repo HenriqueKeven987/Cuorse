4ee0b4122c1c273443c19536b2f6cc62081997cd
HenriqueKeven <henrique.keven@seduc.ce.gov.br>
2021-10-26 16:31:32 -0300

    Data Calendario

--- src/Application/CalendarApp.java
+++ src/Application/CalendarApp.java
@@ -12,14 +12,22 @@ public class CalendarApp {
 		
 		SimpleDateFormat dataFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
-		Date d = Date.from(Instant.parse("2021-10-25T13:59:00Z"));
-		
-		System.out.println(dataFormat.format(d));
+		Date dataEscolhida = Date.from(Instant.parse("2021-10-25T13:59:00Z"));		
 		
 		Calendar calendario = Calendar.getInstance();
 		
+		int dia = calendario.get(calendario.DAY_OF_MONTH);
+		int mes = 1 + calendario.get(calendario.MONTH);
+		int ano = calendario.get(calendario.YEAR);
+		int hora = calendario.get(calendario.HOUR_OF_DAY);
+		int minutos = calendario.get(calendario.MINUTE);
+		int segundos = calendario.get(calendario.SECOND);
+			
 		
-		
+		System.out.println(dataFormat.format(dataEscolhida));	 
+		System.out.println("----------------------------");
+		System.out.println(dia + "/"+ mes + "/"+ ano+ " " + hora + ":" + minutos + ":"+ segundos);
+		 
 	}
 
 }
